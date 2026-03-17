0f9e086a25f15a3ce24fc5906733eacb50456ac7
Mohamed Elsayed <134267921+MoFar20@users.noreply.github.com>
2026-03-17 03:29:01 +0100

    Add CorsFilter import to SecurityConfig.kt

--- src/main/kotlin/de/thm/mni/mailsystem/config/SecurityConfig.kt
+++ src/main/kotlin/de/thm/mni/mailsystem/config/SecurityConfig.kt
@@ -12,6 +12,7 @@ import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import jakarta.servlet.http.HttpServletResponse
 
