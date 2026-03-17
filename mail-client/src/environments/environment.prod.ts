611532f0aac2e7ecc7cd4c4a174a9ec711974dff
Mohamed Elsayed <134267921+MoFar20@users.noreply.github.com>
2026-03-17 04:53:55 +0100

    Remove trailing slash from apiUrl in production config

--- mail-client/src/environments/environment.prod.ts
+++ mail-client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://remarkable-jeanne-thmdms-34e6c67e.koyeb.app/'
+  apiUrl: 'https://remarkable-jeanne-thmdms-34e6c67e.koyeb.app'
 };
